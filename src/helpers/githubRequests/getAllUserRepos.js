433893a98da4a85bc3f61170f56a2bddca41e262
Your New Name <francy.malonga@ginov.io>
2023-08-25 13:10:11 +0100

    Dernier commit

--- src/helpers/githubRequests/getAllUserRepos.js
+++ src/helpers/githubRequests/getAllUserRepos.js
@@ -1,6 +1,9 @@
 export default async function getAllUserRepos(username) {
   try {
-    const resp = await fetch(`https://api.github.com/users/${username}/repos`);
+    const resp = await fetch(
+      `https://api.github.com/users/${username}/repos`
+      // {next:{revalidate: 60 }}
+    );
     const data = resp.json();
     return data;
   } catch (error) {
